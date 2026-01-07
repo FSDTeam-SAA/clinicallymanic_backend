8cc5cbd19e4385da2aafb09dc1d9e8f755007680
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-01-07 08:56:05 +0600

    fix subscription

--- src/app/modules/subscription/subscription.model.ts
+++ src/app/modules/subscription/subscription.model.ts
@@ -7,7 +7,7 @@ const subscriptionSchema = new mongoose.Schema<ISubscription>(
     type: { type: String, required: true, enum: ['monthly', 'yearly'] },
     price: { type: Number, required: true },
     status: { type: String, enum: ['active', 'inactive'] },
-    features: { type: String },
+    features: [{ type: String }],
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     totalSubscribedUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   },
